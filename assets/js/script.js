f33914e95f26983f9cc2a47d267bc6ec5155a4a7
Kathryn.Davies123 <kathryn.davies1233@gmail.com>
2023-01-23 13:29:36 +0100

    header and started js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let menubutton = document.querySelector(".menu-button");
+let navlist = document.querySelector(".navigation");
+let backgroundcolor = document.querySelector(".background-color");
+
+menubutton.addEventListener("click", function () {
+    console.log("test");
+})
