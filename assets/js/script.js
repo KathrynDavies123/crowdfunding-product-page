71f7554165a2b3e57ea6015722f9f3c2b5ead7c3
Kathryn.Davies123 <kathryn.davies1233@gmail.com>
2023-01-23 19:45:45 +0100

    title section almost done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 let menubutton = document.querySelector(".menu-button");
 let navlist = document.querySelector(".navigation");
+
 let backgroundcolor = document.querySelector(".background-color");
 
 menubutton.addEventListener("click", function () {
     backgroundcolor.classList.toggle("background-gradient");
     navlist.classList.toggle("nav-open");
+    menubutton.classList.toggle("close-menu");
 })
