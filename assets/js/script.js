2e46cac4f4ea2719bc4ec404d319507ceec3d243
Kathryn.Davies123 <kathryn.davies1233@gmail.com>
2023-01-27 15:26:35 +0100

    working on modal radios

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,32 @@ let amountoutputdollar = document.querySelector("#amount-backed-dollar");
 let totalbackeresoutput = document.querySelector("#total-backers");
 let progressbar = document.querySelector(".progress-bar-completed");
 
-let productcontainerbamboo = document.querySelector(".bamboo");
-let productcontainerblack = document.querySelector(".black");
-let productcontainermahogany = document.querySelector(".mahogany");
+let productcontainerbamboo = document.querySelectorAll(".bamboo");
+let productcontainerblack = document.querySelectorAll(".black");
+let productcontainermahogany = document.querySelectorAll(".mahogany");
 
-let numberremainingbamboooutput = document.querySelector(
-  "#bamboo-number-remaining"
+let numberremainingbamboooutput = document.querySelectorAll(
+  ".bamboo-number-remaining"
 );
-let numberremainingblackoutput = document.querySelector(
-  "#black-number-remaining"
+let numberremainingblackoutput = document.querySelectorAll(
+  ".black-number-remaining"
 );
-let numberremainingmahoganyoutput = document.querySelector(
-  "#mahogany-number-remaining"
+let numberremainingmahoganyoutput = document.querySelectorAll(
+  ".mahogany-number-remaining"
 );
 
+let openmodalbutton = document.querySelectorAll(".open-modal-button");
+let closemodalbutton = document.querySelector(".close-modal-button");
 let bamboorewardbutton = document.querySelector(".bamboo-button");
 let blackrewardbutton = document.querySelector(".black-button");
 let mahoganyrewardbutton = document.querySelector(".mahogany-button");
+
+let modal = document.querySelector(".modal");
+
+let radiobuttons = document.querySelectorAll("input[type='radio']");
+let bambooradio = document.querySelector("#bamboo-radio");
+let blackradio = document.querySelector("#black-radio");
+let mahoganyradio = document.querySelector("#mahogany-radio");
 
 let amountbackeddollar = 65000;
 let totalbackers = 5007;
@@ -40,27 +49,87 @@ outputStats = () => {
   amountoutputdollar.innerText = `$${amountbackeddollar.toLocaleString()}`;
   totalbackeresoutput.innerText = `${totalbackers.toLocaleString()}`;
   progressbar.style.width = `${amountbackedpercentage}%`;
-  numberremainingbamboooutput.innerText = `${numberremainingbamboo}`;
-  numberremainingblackoutput.innerText = `${numberremainingblack}`;
-  numberremainingmahoganyoutput.innerText = `${numberremainingmahogany}`;
-  if (numberremainingbamboo == 0) {
-    productcontainerbamboo.classList.add("out-of-stock");
-    bamboorewardbutton.innerText = "Out of Stock";
-    bamboorewardbutton.disabled = true;
-  }
-  if (numberremainingblack == 0) {
-    productcontainerblack.classList.add("out-of-stock");
-    blackrewardbutton.innerText = "Out of Stock";
-    blackrewardbutton.disabled = true;
-  }
-  if (numberremainingmahogany == 0) {
-    productcontainermahogany.classList.add("out-of-stock");
-    mahoganyrewardbutton.innerText = "Out of Stock";
-    mahoganyrewardbutton.disabled = true;
-  }
+
+  numberremainingbamboooutput.forEach((item) => {
+    item.innerText = `${numberremainingbamboo}`;
+    if (numberremainingbamboo == 0) {
+      productcontainerbamboo.forEach((item) => {
+        item.classList.add("out-of-stock");
+      })
+
+      bamboorewardbutton.innerText = "Out of Stock";
+      bamboorewardbutton.disabled = true;
+      bambooradio.disabled = true;
+    }
+  })
+
+  numberremainingblackoutput.forEach((item) => {
+    item.innerText = `${numberremainingblack}`;
+    if (numberremainingblack == 0) {
+      productcontainerblack.forEach((item) => {
+        item.classList.add("out-of-stock");
+      })
+      blackrewardbutton.innerText = "Out of Stock";
+      blackrewardbutton.disabled = true;
+      blackradio.disabled = true;
+    }
+  })
+
+  numberremainingmahoganyoutput.forEach((item) => {
+    item.innerText = `${numberremainingmahogany}`;
+    if (numberremainingmahogany == 0) {
+      productcontainermahogany.forEach((item) => {
+        item.classList.add("out-of-stock");
+      })
+      mahoganyrewardbutton.innerText = "Out of Stock";
+      mahoganyrewardbutton.disabled = true;
+      mahoganyradio.disabled = true;
+    }
+  })
 };
 
 outputStats();
+
+openmodalbutton.forEach((item) => {
+  item.addEventListener("click", function () {
+    modal.style.display = "flex";
+    backgroundcolor.classList.add("background-overlay");
+  })
+})
+
+bamboorewardbutton.addEventListener("click", function () {
+  bambooradio.checked = true;
+})
+
+blackrewardbutton.addEventListener("click", function () {
+  blackradio.checked = true;
+})
+
+mahoganyrewardbutton.addEventListener("click", function () {
+  mahoganyradio.checked = true;
+})
+
+closemodalbutton.addEventListener("click", function () {
+  modal.style.display = "none";
+  backgroundcolor.classList.remove("background-overlay");
+  for (let i=0; i<radiobuttons.length; i++) {
+    radiobuttons[i].checked = false;
+    radiobuttons[i].parentElement.classList.remove("product-selected");
+  }
+})
+
+for (let i=0; i<radiobuttons.length; i++) {
+  radiobuttons[i].addEventListener("click", function () {
+    this.parentElement.classList.add("product-selected");
+    this.parentElement.childNodes[9].style.display = "flex";
+    for (let j=0; j<radiobuttons.length; j++) {
+      if (j !== i) {
+        radiobuttons[j].parentElement.classList.remove("product-selected");
+        radiobuttons[j].parentElement.childNodes[9].style.display = "none";
+      }
+    }
+  })
+}
 
 menubutton.addEventListener("click", function () {
   backgroundcolor.classList.toggle("background-gradient");
