f638f24664cc33f8c9125b17238f746f0e05b7e5
“KathrynDavies123” <kathryn.davies1233@gmail.com>
2023-01-30 15:47:03 +0100

    finished js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Declarations
+
 let menubutton = document.querySelector(".menu-button");
 let navlist = document.querySelector(".navigation");
 
@@ -18,29 +20,36 @@ let numberremainingblackoutput = document.querySelectorAll(
 );
 let numberremainingmahoganyoutput = document.querySelectorAll(
   ".mahogany-number-remaining"
-); 
-
-let closemodalbutton = document.querySelector(".close-modal-button");
+);
 
 let rewardbuttons = document.querySelectorAll(".open-modal");
 
 let modal = document.querySelector(".modal");
 
+let defaultmodal = document.querySelector(".modal-default");
+let thankyoumessage = document.querySelector(".thank-you-message");
+
+let closemodalbuttons = document.querySelectorAll(".close-modal");
+
 let radiobuttons = document.querySelectorAll("input[type='radio']");
 
 let continuebuttons = document.querySelectorAll(".continue-button");
 
+// Default numbers
+
 let amountbackeddollar = 65000;
 let totalbackers = 5007;
-let amountbackedpercentage = ((amountbackeddollar / 100000) * 100).toFixed(2);
 
 let numberremainingbamboo = 101;
 let numberremainingblack = 64;
 let numberremainingmahogany = 0;
 
+// Function to output current backed amount, number of backers and number of rewards remaining
+
 outputStats = () => {
   amountoutputdollar.innerText = `$${amountbackeddollar.toLocaleString()}`;
   totalbackeresoutput.innerText = `${totalbackers.toLocaleString()}`;
+  let amountbackedpercentage = ((amountbackeddollar / 100000) * 100).toFixed(2);
   progressbar.style.width = `${amountbackedpercentage}%`;
 
   numberremainingbamboooutput.forEach((item) => {
@@ -79,6 +88,8 @@ outputStats = () => {
 
 outputStats();
 
+// Event listener function to check the radio of the corresponding reward button pressed on the main page in the modal when it opens
+
 rewardbuttons.forEach((item) => {
   item.addEventListener("click", function () {
     let buttonvalue = item.value;
@@ -94,6 +105,8 @@ rewardbuttons.forEach((item) => {
   });
 });
 
+// Function to add style and extra pledge input box when radio is checked
+
 for (let i = 0; i < radiobuttons.length; i++) {
   radiobuttons[i].addEventListener("click", function () {
     this.parentElement.classList.add("product-selected");
@@ -108,6 +121,8 @@ for (let i = 0; i < radiobuttons.length; i++) {
   });
 }
 
+// Function to calculate the new amount pledged, number of backers and number of rewards remaining when an amount is pledged. Also opens the "thank you" message and updates the stats on the main page as long as the conditions for the corresponding rewards are met
+
 continuebuttons.forEach((item) => {
   item.addEventListener("click", function () {
     if (item.value == "bamboo" && item.previousElementSibling.value >= 25) {
@@ -119,20 +134,35 @@ continuebuttons.forEach((item) => {
     if (item.value == "mahogany" && item.previousElementSibling.value >= 200) {
       numberremainingmahogany -= 1;
     }
-    totalbackers += 1;
-    amountbackeddollar += Number(item.previousElementSibling.value);
-    outputStats();
-  })
-})
+    if (
+      (item.value == "bamboo" && item.previousElementSibling.value >= 25) ||
+      (item.value == "black" && item.previousElementSibling.value >= 75) ||
+      (item.value == "mahogany" && item.previousElementSibling.value >= 200) ||
+      (item.value == "noreward" && item.previousElementSibling.value > 0)
+    ) {
+      totalbackers += 1;
+      amountbackeddollar += Number(item.previousElementSibling.value);
+      outputStats();
+      thankyoumessage.style.display = "flex";
+      defaultmodal.style.display = "none";
+    }
+  });
+});
 
-closemodalbutton.addEventListener("click", function () {
-  modal.style.display = "none";
-  backgroundcolor.classList.remove("background-overlay");
-  for (let i = 0; i < radiobuttons.length; i++) {
-    radiobuttons[i].checked = false;
-    radiobuttons[i].parentElement.classList.remove("product-selected");
-    radiobuttons[i].parentElement.childNodes[9].style.display = "none";
-  }
+// Close modal, menu and bookmark buttons
+
+closemodalbuttons.forEach((item) => {
+  item.addEventListener("click", function () {
+    thankyoumessage.style.display = "none";
+    defaultmodal.style.display = "flex";
+    modal.style.display = "none";
+    backgroundcolor.classList.remove("background-overlay");
+    for (let i = 0; i < radiobuttons.length; i++) {
+      radiobuttons[i].checked = false;
+      radiobuttons[i].parentElement.classList.remove("product-selected");
+      radiobuttons[i].parentElement.childNodes[9].style.display = "none";
+    }
+  });
 });
 
 menubutton.addEventListener("click", function () {
